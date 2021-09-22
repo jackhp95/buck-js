a2d2db0a03e5d026a6878c8b333d043363c57ac1
Jack <jackhp95@gmail.com>
2021-09-22 11:12:24 -0500

    prior to npm

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { Graph } from "./graph.js";
+import { log, loop, QSA, asEl, sortEls, invoke } from "./utils.js";
+import { onDom } from "./ondom.js";
+import {
+  extendFunction,
+  batch,
+  recur,
+  orca,
+  sync,
+  then,
+  task,
+  tick,
+  next,
+  idle,
+  wait,
+  once,
+} from "./orca.js";
+
+export {
+  Graph,
+  loop,
+  log,
+  QSA,
+  asEl,
+  sortEls,
+  invoke,
+  onDom,
+  extendFunction,
+  batch,
+  recur,
+  orca,
+  sync,
+  then,
+  task,
+  tick,
+  next,
+  idle,
+  wait,
+  once,
+};
